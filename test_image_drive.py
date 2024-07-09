994c77c2f3d8a2a3edf7f86c7f9c8b868bce9a75
daikidaiku <daiki_daiku@icloud.com>
2024-07-09 16:31:23 +0900

    first commit

---
+++ test_image_drive.py
@@ -0,0 +1,43 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import os
+from dotenv import load_dotenv
+
+# # .envファイルの読み込み
+load_dotenv()
+
+def main():
+    # Google Drive 認証
+    gauth = GoogleAuth()
+    
+    # 認証情報を保存するファイル名
+    credentials_file = os.getenv('GAUTH_CREDENTIALS')
+    # gauth.LocalWebserverAuth()
+    # 認証情報を読み込みます
+    gauth.LoadCredentialsFile(credentials_file)
+    
+    if gauth.credentials is None:
+        # 認証画面を表示します（ブラウザが開かれます）
+        gauth.CommandLineAuth()
+    elif gauth.access_token_expired:
+        # アクセストークンが期限切れの場合、リフレッシュします
+        gauth.Refresh()
+    else:
+        # 既存の認証情報を使用します
+        gauth.Authorize()
+    
+    # 認証情報を保存します
+    gauth.SaveCredentialsFile(credentials_file)
+    
+    drive = GoogleDrive(gauth)
+    
+    # 画像ファイルのアップロード
+    f = drive.CreateFile({"parents":[{'id':os.getenv('PARENT_ID')}],
+                          "id":os.getenv('CHILD_ID'),
+                          "title": "test.jpg",
+                          "mimeType": "image/jpeg"})
+    f.SetContentFile(os.getenv('LAB_IMAGE_PATH'))
+    f.Upload()
+
+if __name__ == "__main__":
+    main()
