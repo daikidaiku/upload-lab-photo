de761cc90ee40ef9a2cbe761fae0136d0dcabcab
daikidaiku <daiki_daiku@icloud.com>
2024-07-18 21:20:22 +0900

    認証方法を変更

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+from oauth2client.service_account import ServiceAccountCredentials
+
+my_dir = Path(__file__).parent
 
 # # .envファイルの読み込み
 load_dotenv()
@@ -9,28 +13,35 @@ load_dotenv()
 def main():
     # Google Drive 認証
     gauth = GoogleAuth()
-    
+    scope = ["https://www.googleapis.com/auth/drive"]
+    gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(os.getenv('SECRETS_PATH'), scope)
+
     # 認証情報を保存するファイル名
-    credentials_file = os.getenv('GAUTH_CREDENTIALS')
+    # credentials_file = os.getenv('SECRETS_PATH')
     
-    #認証情報が存在しない場合は以下を実行する必要があります
+    # gauth.settings_file = os.getenv('YAML_PATH')
+    
+# #認証情報が存在しない場合は以下を実行する必要があります
     # gauth.LocalWebserverAuth()
     
-    # 認証情報を読み込みます
-    gauth.LoadCredentialsFile(credentials_file)
+    # # 認証情報を読み込みます
+    # gauth.LoadCredentialsFile(credentials_file)
+    # gauth.CommandLineAuth()
+    # gauth.LocalWebserverAuth()
+    # gauth.ServiceAuth()
     
-    if gauth.credentials is None:
-        # 認証画面を表示します（ブラウザが開かれます）
-        gauth.CommandLineAuth()
-    elif gauth.access_token_expired:
-        # アクセストークンが期限切れの場合、リフレッシュします
-        gauth.Refresh()
-    else:
-        # 既存の認証情報を使用します
-        gauth.Authorize()
+    # if gauth.credentials is None:
+    #      # 認証画面を表示します（ブラウザが開かれます）
+    #      gauth.CommandLineAuth()
+    # elif gauth.access_token_expired:
+    # #     # アクセストークンが期限切れの場合、リフレッシュします
+    #     gauth.Refresh()
+    # else:
+    # #     # 既存の認証情報を使用します
+    #     gauth.Authorize()
     
-    # 認証情報を保存します
-    gauth.SaveCredentialsFile(credentials_file)
+    # # 認証情報を保存します
+    # gauth.SaveCredentialsFile(credentials_file)
     
     drive = GoogleDrive(gauth)
     
